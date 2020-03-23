95fd3eb50deeaaf5f12e021d26e56f9c84f7958f
TheresaLiao <33051006+TheresaLiao@users.noreply.github.com>
2020-03-23 09:42:25 +0800

    Create samba_server.sh

---
+++ samba_server.sh
@@ -0,0 +1,5 @@
+docker run -it -p 139:139 -p 445:445 -p 137:137/udp \
+-p 138:138/udp --name samba \
+-v /deeplook_media/:/deeplook_media \
+-v /Deeplook_k8s/Deploy/Samba/conf/:/etc/samba/ \
+-d dperson/samba -s "public;/share"
