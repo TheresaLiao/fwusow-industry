d8b6c01e2a727c2e5f382faf97eefe267762b417
TheresaLiao <33051006+TheresaLiao@users.noreply.github.com>
2020-03-23 10:32:55 +0800

    Update nginx_server.sh

--- nginx_server.sh
+++ nginx_server.sh
@@ -1,6 +1,7 @@
 docker run -ti -d \
+--restart=always \
 --name=nginx \
 -v /deeplook_media:/usr/share/nginx/html/exports \
 -v /Deeplook_k8s/Deploy/nginx/nginx.conf:/root/tmp_config/nginx.conf \
--p 80:80 \
+-p 10000:80 \
 nginx:1.7.9
