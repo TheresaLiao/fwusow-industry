58b6b93a528d79c16db640fefdb74c1776ec621d
TheresaLiao <33051006+TheresaLiao@users.noreply.github.com>
2020-06-04 15:26:05 +0800

    Update samba_server.sh

--- samba_server.sh
+++ samba_server.sh
@@ -4,3 +4,8 @@ docker run -it -p 139:139 -p 445:445 -p 137:137/udp \
 -v /deeplook_media/:/deeplook_media \
 -v /Deeplook_k8s/Deploy/Samba/conf/:/etc/samba/ \
 -d dperson/samba -s "public;/share"
+
+
+$ docker run -it -p 139:139 -p 445:445 -p 137:137/udp -p 138:138/udp --name samba \
+-v /deeplook_media/:/deeplook_media -v /Deeplook_k8s/Deploy/Samba/conf/:/etc/samba/  \
+-d dperson/samba -s "public;/share"
